22fa6d857edcf9884caf254c862217549c0dfe7e
luis- <luis-@DESKTOP-5VG1NHG>
2019-06-02 14:31:59 +0200

    "Fin"

--- src/cd_sqlite_proyecto/Cd_sqlite_proyecto.java
+++ src/cd_sqlite_proyecto/Cd_sqlite_proyecto.java
@@ -1,16 +1,15 @@
-
 package cd_sqlite_proyecto;
-
 
 public class Cd_sqlite_proyecto {
 
-   
+    /**
+     * main donde se llamará a los métodos de la clase BD para interactuar con
+     * la BD.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        
-        
-        
-        
+
     }
-    
-    
+
 }
